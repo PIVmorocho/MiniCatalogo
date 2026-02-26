5b3543a2e3e5951753b6d73d4fdf136d3720a77a
PIVmorocho <erikespe7@gmail.com>
2026-02-26 12:15:49 -0500

    tercer cometario

--- MiniCatalogo/Models/ErrorViewModel.cs
+++ MiniCatalogo/Models/ErrorViewModel.cs
@@ -7,6 +7,7 @@ namespace MiniCatalogo.Models
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         //comentario de priueba pedro valente, esta linea 8 es la que se agrega a mi repositorio, con mi usuario
         //segundo comentario de prueba, para confirmar los cambios de sincronización
+        //Views/Producto/Create.cshtml
     }
 
 } 
