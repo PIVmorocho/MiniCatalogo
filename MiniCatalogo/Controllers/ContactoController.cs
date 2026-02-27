bbd60008ede8e212bc2d132661ac9f535fde148e
PIVmorocho <erikespe7@gmail.com>
2026-02-26 23:38:05 -0500

    Agregar la vista de contacto

---
+++ MiniCatalogo/Controllers/ContactoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCatalogo.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
